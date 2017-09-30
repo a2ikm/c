7b191504cd8184bbc520fef924706b6a834bf8ed
Masato Ikeda <masato.ikeda@gmail.com>
2017-10-01 06:06:51 +0900

    cat from stdin

--- cat/main.c
+++ cat/main.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void do_cat(const int argc, const char *argv[]);
+void put_stream(FILE *f);
+
 int
 main(const int argc, const char *argv[])
+{
+  if (argc == 1) {
+    put_stream(stdin);
+  } else {
+    do_cat(argc, argv);
+  }
+  exit(1);
+}
+
+void
+do_cat(const int argc, const char *argv[])
 {
   int i;
   for (i = 1; i < argc; i++) {
     FILE *f;
-    int c;
-
     f = fopen(argv[i], "r");
     if (!f) {
       perror(argv[i]);
       exit(1);
     }
-    while ((c = fgetc(f)) != EOF) {
-      if (putchar(c) < 0) exit(1);
-    }
+    put_stream(f);
     fclose(f);
   }
-  exit(1);
+}
+
+void
+put_stream(FILE *f)
+{
+  int c;
+  while ((c = fgetc(f)) != EOF) {
+    if (putchar(c) < 0) exit(1);
+  }
 }
